304724ce965d65e382f0f81b464db4019515d652
rayoungji <dudwl7676@naver.com>
2020-04-26 13:34:52 +0900

    mainrouter 수정

--- routers/main/main.js
+++ routers/main/main.js
@@ -1,13 +1,15 @@
-var express=require('express')
-var router=express.Router()
-var path= require('path') 
+var express = require('express')
+var app = express()
+var router = express.Router()
+var path = require('path')
 
-router.get('/',function(req,res){
-    console.log('라우트 분리 성공-')
-    console.log("main: ",req.user)
-    console.log(res.user)
-    var id=req.user
-    res.render('main.ejs',{'id':id})
-})
+//main page 는 login이 될 때만(즉 세션정보가 있을때만) 접근이 가능하게 하자.
+router.get('/', function(req,res) {
+	console.log("main loaded")
+	console.log("회원가입 성공시 req.user가 있을까요?",req.user)
+	var id = req.user;
+	//if(!id) res.render('login.ejs');
+	res.render('main.ejs', {'id' : id});
+});
 
-module.exports=router; 
+module.exports = router;
