f616e75f0f2ae33a2db6df16b726759c8a59e502
rayoungji <dudwl7676@naver.com>
2020-04-26 12:55:41 +0900

    logout 구현

--- routers/index.js
+++ routers/index.js
@@ -6,11 +6,13 @@ var main=require('./main/main')
 var mail=require('./mail/mail')
 var join=require('./join/join')
 var login=require('./login/login')
+var logout=require('./logout/logout')
 
 //라우터 레벨에 미들웨어 정의
 router.use('/main',main)
 router.use('/mail',mail)
 router.use('/join',join)
 router.use('/login',login)
+router.use('/logout',logout)
 
 module.exports=router;
